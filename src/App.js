01d90c12bc4562a64bf67f66aeadb06c617b0117
cetinsangu <sangucetin@gmail.com>
2022-06-27 11:45:13 +0300

    tailwind prettier plugin installed

--- src/App.js
+++ src/App.js
@@ -32,15 +32,15 @@ function App() {
   answers.splice(randomIndex, 0, correct_answer);
 
   return (
-    <div className="font-poppins flex justify-center items-center h-screen w-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-      <div className="rounded-xl relative container w-4/5  md:w-full  mx-auto bg-white bg-opacity-40 backdrop-blur-lg p-5 flex justify-center items-center">
+    <div className="flex h-screen w-screen items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 font-poppins">
+      <div className="container relative mx-auto flex w-4/5 items-center justify-center rounded-xl bg-white bg-opacity-40 p-5 backdrop-blur-lg md:w-full">
         <div className="absolute right-0 top-0 mr-5 mt-3 ">
           Correct Answers: {`${correctAnswer}/${currentQuestion}`}
         </div>
         <div className="flex flex-col items-center justify-center">
           <h1
             dangerouslySetInnerHTML={{ __html: question }}
-            className={`text-center text-2xl text-black my-10`}
+            className={`my-10 text-center text-2xl text-black`}
           />
           {answers.map((answer, index) => {
             return (
@@ -50,14 +50,14 @@ function App() {
                   checkAnswer(answer);
                 }}
                 dangerouslySetInnerHTML={{ __html: answer }}
-                className="mt-5 w-2/3 px-5 py-3 rounded-xl text-sm font-medium text-indigo-600 bg-white outline-none m-1  border-indigo-600 hover:border-2 hover:border-indigo-800 hover:text-indigo-800 active:border-grey-900 active:text-grey-900 transition-all"
+                className="active:border-grey-900 active:text-grey-900 m-1 mt-5 w-2/3 rounded-xl border-indigo-600 bg-white px-5 py-3 text-sm  font-medium text-indigo-600 outline-none transition-all hover:border-2 hover:border-indigo-800 hover:text-indigo-800"
               ></button>
             );
           })}
-          <div className="w-full flex justify-end">
+          <div className="flex w-full justify-end">
             <button
               onClick={nextQuestion}
-              className="mt-8 px-5 py-3 flex items-center justify-center rounded-xl text-sm font-medium text-black bg-yellow-300 hover:bg-yellow-500 active:bg-grey-900 focus:outline-none transition-all"
+              className="active:bg-grey-900 mt-8 flex items-center justify-center rounded-xl bg-yellow-300 px-5 py-3 text-sm font-medium text-black transition-all hover:bg-yellow-500 focus:outline-none"
             >
               Next Question
               <GrLinkNext style={{ marginLeft: 5 }} />
